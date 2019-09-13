f0d34f77d6620c7fe95dfdb330e51b1e02ced873
Guilherme Vieira <guilherme@feracode.com>
2019-09-13 12:28:30 -0300

    Added mustache

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import easy from 'easy-oauth2'
 import express from 'express'
 import server from 'http'
+import mustacheExpress from 'mustache-express'
+
 const app = express()
 
+app.engine('mst', mustacheExpress())
+app.set('view engine', 'mst')
+app.set('views', __dirname + '/views')
 
-let serverImpl = server.createServer(app)
+let easyInstance = new easy(app)
 
-let server = easy.createServer()
+let applications = []
+easyInstance.saveApplication = async function(application) {
+	applications.push(application)
+	console.log(applications)
+}
+
+easyInstance.renderAuthorizationView = async function(req, res, next) {
+	res.render('main', { oie: 'Batata frita é mt bom' + req.query.teste })
+}
+
+easyInstance.initViews()
+
+let serverInstance = server.createServer(app)
+
+serverInstance.listen(3000, () => {
+	console.log('listening')
+})
